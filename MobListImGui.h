cba84aef9a743e54f8899bcbb4e05b257477a084
Lisie <lc.x@tutanota.com>
2024-11-16 23:41:04 -0600

    Removed MobListImGui class
    Removed class and eColumnId enum, moved constexpr to cpp file.  Added declarations for ImGui draw functions.

--- MobListImGui.h
+++ MobListImGui.h
@@ -1,44 +1,10 @@
 #pragma once
 #include "MQMobList.h"
 #include <mq/Plugin.h>
-#include <mq/imgui/Widgets.h>
 
-#define ARROW_WIDTH 5
-#define ARROW_HEIGHT 15
-#define ARROW_SIZE 25
-
-extern bool showMobListWindow;
-
-enum eColumnId {
-	Id,
-	Lvl,
-	DisplayName,
-	Name,
-	Distance,
-	Loc,
-	Body,
-	Race,
-	Class,
-	Direction
-};
-
-class MobListImGui
-{
-public:
-	static void drawDirectionalArrow(SPAWNINFO* spawn, ImVec2 cursorPos);
-	static void drawMobRow(SPAWNINFO* spawn);
-	static void drawMobList(std::vector<SPAWNINFO*>& spawnList);
-	static void drawMobListTable(std::vector<SPAWNINFO*>& spawnList);
-	static void drawSearchHeader();
-	static void drawOptMenu();
-	static constexpr int FIRST_WINDOW_WIDTH = 445;
-	static constexpr int FIRST_WINDOW_HEIGHT = 490;
-
-
-	static constexpr int tableFlags = (ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable | ImGuiTableFlags_RowBg 
-		| ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable | ImGuiTableFlags_Sortable | ImGuiTableFlags_ScrollY | ImGuiTableFlags_SizingFixedFit);
-};
-
-namespace ImGui {
-	IMGUI_API bool InputText(const char* label, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback = nullptr, void* user_data = nullptr);
-}
+void drawMobList(std::vector<SPAWNINFO*>& spawnList, Filters& filters);
+void drawMenu(Filters& filters);
+void drawSearchHeader(Filters& filters);
+void drawMobListTable(std::vector<SPAWNINFO*>& spawnList, Filters filters);
+void drawMobRow(SPAWNINFO* spawn, Filters filters);
+void drawDirectionalArrow(SPAWNINFO* spawn, ImVec2 cursorPos);
